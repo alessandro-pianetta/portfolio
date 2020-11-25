fda37030097e6fc3ee753578b102626081e67321
alessandro-pianetta <alessandro.pianetta@gmail.com>
2020-11-25 05:57:51 -0800

    Configures webpack, eslint, prettier

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  entry: { index: path.resolve(__dirname, 'src', 'index.js') },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ['babel-loader'],
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.html$/,
+        loader: 'html-loader',
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'src', 'index.html'),
+    }),
+  ],
+  devServer: {
+    hot: true,
+  },
+};
